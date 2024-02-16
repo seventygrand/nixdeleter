a93a8fe6194bc3fa5a825457e81adf660797bd88
seventygrand <155555829+seventygrand@users.noreply.github.com>
2024-02-16 12:23:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('linkButton').addEventListener('click', function() {
+  window.location.href = 'https://drive.google.com/drive/folders/1lB7MQQ__jW1A5oVnAo-OJzuGxbEdIgJC?usp=sharing';
+});
